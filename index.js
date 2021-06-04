8522c6f21fa0308cb7448a20fb8a94aa0660628d
karl-thomas <karl.thomas925@gmail.com>
2021-06-04 08:53:49 -0500

    yes error

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const { build } = require("esbuild");
+const { sassPlugin } = require("esbuild-sass-plugin");
 
 build({
   entryPoints: ["src/index.css"],
   bundle: true,
   outdir: "dist",
+  plugins: [sassPlugin()],
 });
