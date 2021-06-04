199521786fd633363f4b25e372a42b899249b899
karl-thomas <karl.thomas925@gmail.com>
2021-06-04 08:54:35 -0500

    still error
    changed to scss file

--- index.js
+++ index.js
@@ -2,8 +2,11 @@ const { build } = require("esbuild");
 const { sassPlugin } = require("esbuild-sass-plugin");
 
 build({
-  entryPoints: ["src/index.css"],
+  entryPoints: ["src/index.scss"],
   bundle: true,
   outdir: "dist",
   plugins: [sassPlugin()],
+  external: [
+    "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+  ],
 });
